fdef6a41deb78064b9e9221688308569d479c6a7
root <root@XL434.localdomain>
2025-12-03 14:07:17 +0300

    ip addr show eth0

--- egtsFunc.h
+++ egtsFunc.h
@@ -28,10 +28,11 @@
 //--------------------------------------------------------------------
 
 
-#define buf_size 8192
+#define buf_size 16384
 #define TIME_STR_LEN 64
 #define resp_size 1024
-#define vrem_size buf_size << 1
+//#define vrem_size buf_size << 1
+#define vrem_size 8192    // Increased
 #define max_data_wait 32//16//180
 #define size_imei 15
 #define size_imsi 16
