812ad7cbb5d99f6da89f9ee5f52d1f944421a7ae
rg3443 <verna.boris.al@gmail.com>
2025-12-19 17:45:58 +0300

    debugging...

--- egtsFunc.c
+++ egtsFunc.c
@@ -903,10 +903,10 @@ uint16_t calc_CRC16 = CRC16EGTS(from_cli, flen - 2);
                                     //}
                                     sprintf(srst+strlen(srst), "\n");
                                     uki += dl;
+                                    printf("term_id is: %d", term_id->TID);
                                     printf("pos_data4 %d\n", mysqlConnected);
 
                                     if(mysqlConnected) {
-                                        printf("term_id is: %d", term_id->TID);
                                         SQLQuerryPosData(conn_,term_id,sr_pos_data);
                                     }
                                     printf("pos_data5\n");
